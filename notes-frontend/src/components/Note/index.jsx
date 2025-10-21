d70438f8a6f809c6a2ed699b58646b933c15743f
Ruan Cesar <ruancos@al.insper.edu.br>
2025-10-21 09:17:29 -0300

    feat: Formulario criado e botão deletar notas introduzido

--- notes-frontend/src/components/Note/index.jsx
+++ notes-frontend/src/components/Note/index.jsx
@@ -1,9 +1,24 @@
+import axios from "axios";
 import "./index.css";
 
 export default function Note(props) {
+  const deletarNota = () => {
+    axios
+      .delete(`http://localhost:8000/api/notes/${props.id}`)
+      .then(() => {
+        props.loadNotes();
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="card">
-      <h3 className="card-title">{props.title}</h3>
+      <div className="card-header">
+        <h3 className="card-title">{props.title}</h3>
+        <button className="delete-btn" onClick={deletarNota}>
+          ✕
+        </button>
+      </div>
       <div className="card-content">
         <p>{props.children}</p>
       </div>
