09dda7e1897799724a41f5a114735f5010901e69
Ruan Cesar <ruancos@al.insper.edu.br>
2025-10-21 08:02:23 -0300

    feat: Inicio da etapa Implementando a Criação de Anotações

---
+++ notes-frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Note from "./components/Note";
+import AppBar from "./components/AppBar/Index";
+import "./App.css";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/notes/")
+      .then((res) => setNotes(res.data));
+  }, []);
+
+  console.log(notes);
+
+  return (
+    <>
+    <AppBar />
+    <main className="container">
+      <div className="card-container">
+      {notes.map((note) => (
+        <Note key={`note__${note.id}`} title={note.title}>
+          {note.content}
+        </Note>
+      ))}
+      </div>
+    </main>
+    </>
+  );
+}
+
+export default App;
---
+++ notes-frontend/src/components/AppBar/index.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+
+export default function AppBar() {
+    return (
+        <div className="app-bar">
+            <img src="/logo-getit.png" className="app-bar-logo"/>
+            <span className="subtitle">Como o Post-it, mas com outro verbo</span>
+        </div>
+    );
+}
